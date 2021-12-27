59d878282270981389695fc51a9b3df31209dae7
Maycol <42551238+Sorbete-Sempai123@users.noreply.github.com>
2021-12-27 15:36:04 -0500

    Last Changes

--- test_slic.cpp
+++ test_slic.cpp
@@ -22,34 +22,30 @@ using namespace std;
 
 
 int main(int argc, char *argv[]) {
-    /* Load the image and convert to Lab colour space. */
-    char* file_path = "dog.png";
-    Image img(file_path);
     
-    //cvCvtColor(image, img, CV_BGR2Lab);
-    
-    /* Yield the number of superpixels and weight-factors from the user. */
+        
     int nr_superpixels = atoi(argv[1]);
     int nc = atoi(argv[2]);
-    
+    char*  file_path = (argv[3]);
+
+    Image img(file_path);
+
     double step = sqrt((img.width * img.height) / (double) nr_superpixels);
     
-    // cout << nr_superpixels << " " << nc << endl;
-    // cout << img.channels << " " << img.width << " " << img.height << endl;
-
-    /* Perform the SLIC superpixel algorithm. */
     Slic slic;
 
     slic.initialize(&img, step, nc);
+
+    
 
     double *global_centers= new double[slic.getCenters() *5]{-1};
     for (int i = 0; i < slic.getCenters() *5; i++)
     {
         global_centers[i]=-1;
     }
+
+    slic.centersToArray(global_centers);
     
-    //slic.generate_superpixels(&img, step, nc); // por verse xd
-    // inicializa todo ----
   	int my_rank;  
 	int p;	
 	int i;
@@ -57,14 +53,23 @@ int main(int argc, char *argv[]) {
 	MPI_Comm_size(MPI_COMM_WORLD, &p);
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-    int *local_clusters= new int[img.width*img.height]{-1};
+
+    if (my_rank == 0 ){
+        cout << " centros " << slic.getCenters() <<  endl;
+    }
+
+
+    int *local_clusters= new int[img.width*img.height];
     for (int i = 0; i < img.width*img.height; i++)
     {
         local_clusters[i] = -1;
     }
     //Leer imagen
     int dim[3];
-    //Iteraciones
+
+
+
+    //Iteraciones del algoritmo superpixel
     for (int it = 0; it < 10; it++)
     {
         float *local_distances=new float[img.width*img.height];
@@ -79,8 +84,11 @@ int main(int argc, char *argv[]) {
             local_centers[i]=-1;
         }
 
-            slic.centersToArray(global_centers);
+        // Se obtienen los ultimos valores de los centros 
+        slic.centersToArray(global_centers);
 
+
+        //Se distribuyen los centros a ser procesados mediante un sccater
         MPI_Scatter(global_centers,div,MPI_DOUBLE,local_centers,div, MPI_DOUBLE,0,MPI_COMM_WORLD);
         
         // A trabajal
@@ -90,40 +98,33 @@ int main(int argc, char *argv[]) {
         int number_centers = (my_rank + 1 != p ) ? div/5 : (div/5 - slic.getCenters()%p);
         number_centers = div/5;
         number_centers = slic.getCenters();
-        // cout << number_centers << endl;
-        // cout << "div : "<<div<< "   div*p: " <<div*p << endl;
-        // total centros = 408
-        // 408 / 4  = 102  * 5 = 510 
-        // 0 - >        
-        // 1
-        // 2 
-        // 102 - 408%4
-        //Mal p, number of centers
+
+        // cada proceso calcula sus distancias y clusters
         calculate_superpixel(&img, step, nc , 
                                 local_centers,div, 
                                local_distances, local_clusters);
         
 
-        // a juntal 
+        // Se juntan los resultados mediante un all to one en estructura de arbol log(p)
         int i = 0;
         while (true){
             if ( int(my_rank/(pow(2,i))) % 2 != 0 ){ // Envia
-                // cout << "SOY: "<<my_rank<< " Y le envio a: " << my_rank-pow(2,i) << endl;
+                
+                // nodo que envia sus resultados a los nodos en forma de arbol y espera la siguiente iteracion 
                 MPI_Send(local_distances,img_tam,MPI_FLOAT,my_rank-pow(2,i),my_rank,MPI_COMM_WORLD); // mandar al rank - 2^i               n
                 MPI_Send(local_clusters,img_tam,MPI_INT,my_rank-pow(2,i),my_rank,MPI_COMM_WORLD); // mandar al rank - 2^i               n
                 
-                break;// >:v
+                break;
             }
             else{
                 if (my_rank+pow(2,i) < p){
+                    // nodo que recibe la informacion de otro nodo realiza sus calculos y realiza su siguiente iteracion en el arbol
                     float *external_distances= new float[img.width*img.height];
                     int   *external_clusters= new int[img.width*img.height];
                     MPI_Recv(external_distances,img_tam,MPI_FLOAT,my_rank+pow(2,i),my_rank+pow(2,i),MPI_COMM_WORLD, MPI_STATUS_IGNORE); // RECIBIR DEL rank + 2^i      n -> 2n
                     MPI_Recv(external_clusters,img_tam,MPI_INT,my_rank+pow(2,i),my_rank+pow(2,i),MPI_COMM_WORLD, MPI_STATUS_IGNORE); // RECIBIR DEL rank + 2^i      n -> 2n
                     for (int i = 0; i < img_tam; i++){
-                        // if(external_distances[i]==0){
-                        //     cout <<"porotoalerta"<<endl;
-                        // }
+                        // se mantienen los minimos entre los dos nodos
                         if(external_distances[i]<local_distances[i]){
                             local_distances[i]=external_distances[i];
                             local_clusters[i]= external_clusters[i];
@@ -137,10 +138,12 @@ int main(int argc, char *argv[]) {
             } 
             i++;
         }
+
+        
         MPI_Barrier(MPI_COMM_WORLD);
 
-        //Actualizar los centros
-        if ( my_rank == 0){
+        // se realiza la operacion de calcular los nuevos centros con los clusters finales 
+        if ( my_rank == 0 && it == 0){
             slic.updateClusters(local_clusters,img.width,img.height);
             slic.recalculate_centers(&img);
         }
@@ -151,28 +154,18 @@ int main(int argc, char *argv[]) {
         delete[] local_distances;
 
     }
-
+    delete [] local_clusters;
     MPI_Barrier(MPI_COMM_WORLD);
+
+    // re conectan los centros finales y de guarda la iamgen resultante
     if(my_rank==0){
 
-    slic.create_connectivity(&img);
-    /* Display the contours and show the result. */
+        slic.create_connectivity(&img);
+        /* Display the contours and show the result. */
 
-    slic.display_contours(&img, Color(255,0,0));
-    img.write("result.png");
+        slic.display_contours(&img, Color(255,0,0));
+        img.write("result.png");
     }
     MPI_Finalize();
-    
-    //  Enviar centros a los procesos
-    //  Calcular distancia y clusters
-    //  Recolectar distancias y clusters modo arbol xd
-    // Nodo master calcular centros
-    // Reenviar 
-
-    // slic.create_connectivity(&img);
-    // /* Display the contours and show the result. */
-
-    // slic.display_contours(&img, Color(255,0,0));
-    // img.write("result.png");
 
 }
